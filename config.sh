0fa1f5794281a2228947a5c31b67f1596b2bc22d
asdf <puta@que.pariu>
2014-02-08 15:54:13 -0200

    cria o backup apenas quando necessario

--- config.sh
+++ config.sh
@@ -4,13 +4,14 @@ dir=`pwd`
 foundsdir="$dir/dotfiles_found"
 files="vimrc bash_profile aliases"
 
-mkdir -p $foundsdir
-
 cd $dir
 for file in $files; do
 	if [ -h ~/.$file ]; then
 		unlink ~/.$file
 	elif [ -f ~/.$file ]; then
+		if [ -d $foundsdir ]; then
+			mkdir $foundsdir
+		fi
 	    mv ~/.$file $foundsdir
 	fi
 
